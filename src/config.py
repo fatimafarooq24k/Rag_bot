aa1b8a53e23a92a177c266c1d2edc5b51bedec3a
Fatima Farooq <fatimafarooq24k@gmail.com>
2026-07-27 13:02:11 +0500

    chore: Modified paths

--- src/config.py
+++ src/config.py
@@ -1,7 +1,7 @@
 DATA_FOLDER = "data"
 
-INDEX_PATH = "data/index.faiss"
-CHUNKS_PATH = "data/chunks.pkl"
+INDEX_PATH = "data/processed/index.faiss"
+CHUNKS_PATH = "data/processed/chunks.pkl"
 
 HNSW_M = 32
 HNSW_EF_CONSTRUCTION = 200
