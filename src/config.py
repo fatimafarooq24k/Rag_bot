1d31b029b6e0c3913c2b9d9a1e39fc63494c1804
Fatima Farooq <fatimafarooq24k@gmail.com>
2026-07-30 14:18:14 +0500

    update: added new constants

--- src/config.py
+++ src/config.py
@@ -1,3 +1,8 @@
+from dotenv import load_dotenv
+import os
+
+load_dotenv()
+
 DATA_FOLDER = "data"
 
 INDEX_PATH = "data/processed/index.faiss"
@@ -5,3 +10,17 @@ CHUNKS_PATH = "data/processed/chunks.pkl"
 
 HNSW_M = 32
 HNSW_EF_CONSTRUCTION = 200
+# Retriever Configuration
+
+TOP_K = 12
+
+# Generator Configurations
+
+GROQ_API_KEY = os.getenv("GROQ_API_KEY")
+
+if GROQ_API_KEY is None or GROQ_API_KEY == "":
+    raise ValueError("No API key found.")
+
+MODEL = "openai/gpt-oss-20b"
+TEMPERATURE = 0.3
+MAX_TOKENS = 512
